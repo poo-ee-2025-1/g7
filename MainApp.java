d9ae65165ea18e609ce08109d0e32dde13081416
GustavoLadislauMachado <gustavo.machado@discente.ufg.br>
2025-06-02 23:55:46 -0300

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainApp extends Application {
+public class MainApp extends outras.classes {
 
     @Override
     public void start(Stage primaryStage) {
